3c4b4af794a72b3ba744e41fb77d51b4bae77831
gary <gary.white@lodgit.com>
2021-07-06 16:05:43 +0200

    code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Kata-Quarter-of-the-year
+
+From the codewars Kata: Quarter of the year
--- kataQuarter.js
+++ kataQuarter.js
@@ -1,10 +1,18 @@
 const quarterOf = (month) => {
     // Your code here
-    
+    if (month <= 3) {
+        return 1;
+    } else if (month <= 6) {
+        return 2;
+    } else if (month <= 9) {
+        return 3;
+    } else {
+        return 4;
+    }
   }
 
 
 
-console.log(quarterOf(3))
-console.log(quarterOf(8))
-console.log(quarterOf(11))
+console.log(quarterOf(3)) // return 1
+console.log(quarterOf(8)) // return 3
+console.log(quarterOf(11)) // return 4
